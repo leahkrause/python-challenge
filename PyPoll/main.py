43c45aea892026b6965f8fccb01165b648793cb2
leahkrause <leahrkrause@gmail.com>
2022-12-28 15:59:25 -0800

    Code and output analysis

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,61 +2,71 @@
 import os
 
 # import csv module
-import csv 
+import csv
 
 # Path to CSV file
 csvpath = os.path.join('/Users/leahkrausetp/Desktop/Leah_UCB/PROJECTS/python-challenge/PyPoll','Resources', 'election_data.csv')
 
-# Define any outside variables
-total_votes = 0
-
 # Read CSV file in "read" mode. Assign readable file as a new variable
 with open(csvpath) as csvfile:
 
-    # Specifies delimiter and variable. Prints this into csv reader 
+# Specifies delimiter and variable. Prints this into csv reader
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    # Read header and skips it
+# Read header and skips it
     header = next(csvreader)
-    first_row = next(csvreader)
 
-    # Create lists to store data
+# Create lists to store data
     voter_id = []
     candidate = []
     candidates_votes = []
+    indv_candidates = []
 
-    # Create variables for three places 
-    winner = 0
-    runner_up = 0
-    third_place = 0 
+# Create variables
+    can1_votes = 0
+    can2_votes = 0
+    can3_votes = 0
+    total_votes = 0
 
-     # Attain individual candidates names without repetition 
-    indv_candidates = list(set(candidate))
-
-    # Create a function that provides the total number and percentage each candidate received 
+# Create a function that provides the total number and percentage each candidate received
     for row in csvreader:
-        
-        # Total votes
+
+# Total votes
         voter_id.append((row[0]))
-        
-        # Candidate list
+# Candidate list
         candidate.append(row[2])
 
-        # Count up votes per candidate
-        if row == indv_candidates[0]:
-            winner += 1
-        elif row == indv_candidates[1]:
-            runner_up += 1
-        elif row == indv_candidates[2]:
-             third_place += 1 
+# Attain individual candidates names without repetition
+    indv_candidates = list(set(candidate))
 
-    # Calculate total number of votes
-    total_votes = len(voter_id)
+    with open(csvpath) as csvfile2:
+        csvreader2 = csv.reader(csvfile2, delimiter=',')
+        header = next(csvreader2)
 
-   # Voter percentage per candidate
-    winner_percentage = round(winner / total_votes * 100, 3) 
-    runner_up_percentage = round(runner_up / total_votes * 100, 3) 
-    third_place_percentage = round(third_place / total_votes * 100, 3) 
+        for row in csvreader2:
+# Count up votes per candidate
+            if row[2] == indv_candidates[0]:
+                can1_votes += 1
+            elif row[2] == indv_candidates[1]:
+                can2_votes += 1
+            elif row[2] == indv_candidates[2]:
+                can3_votes += 1
+
+# Calculate total number of votes
+        total_votes = len(voter_id)
+
+# Voter percentage per candidate
+        can1_percentage = round(can1_votes / total_votes * 100, 3)
+        can2_percentage = round(can2_votes / total_votes * 100, 3)
+        can3_percentage = round(can3_votes / total_votes * 100, 3)
+
+# Winner 
+if can1_percentage > can2_percentage and can3_percentage:
+    winner = indv_candidates[0]
+elif can2_percentage > can3_percentage and can1_percentage:
+    winner = indv_candidates[1]
+elif can3_percentage > can2_percentage and can1_percentage:
+    winner = indv_candidates[2]
 
 #################################################################
 
@@ -65,5 +75,9 @@ print("Election Results")
 print("--"*15)
 print("Total Votes: ", total_votes)
 print("--"*15)
-print()
-
+print(f"{indv_candidates[0]}: {can1_percentage}% ({can1_votes})")
+print(f"{indv_candidates[1]}: {can2_percentage}% ({can2_votes})")
+print(f"{indv_candidates[2]}: {can3_percentage}% ({can3_votes})")
+print("--"*15)
+print(f"Winner: {winner}")
+print("--"*15)
